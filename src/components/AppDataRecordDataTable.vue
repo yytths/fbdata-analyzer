f5453a74e213b78d379497d29c9edaf3092fcaa5
yytths <hmdob.arch@gmail.com>
2020-05-22 12:45:40 +0900

    modify: フォーマットを全銀に統一 (#4)

--- src/components/AppDataRecordDataTable.vue
+++ src/components/AppDataRecordDataTable.vue
@@ -19,44 +19,31 @@ export default {
 
   computed: {
     records() {
-      return this.items.map((record) => {
-        const obj = {
-          dataPartition: record.substr(0, 1),
-          destinationFinancialInstitutionNumber: record.substr(1, 4),
-          destinationFinancialInstitutionName: record.substr(5, 15),
-          destinationBranchNumber: record.substr(20, 3),
-          destinationBranchName: record.substr(23, 15),
-          clearingHouseNumber: record.substr(38, 4),
-          depositItem: record.substr(42, 1),
-          accountNumber: record.substr(43, 7),
-          beneficiaryName: record.substr(50, 30),
-          transferAmountOfMoney: record.substr(80, 10),
-          newCode: record.substr(90, 1),
-          customerCodeOne: record.substr(91, 10),
-          customerCodeTwo: record.substr(101, 10),
-          EDIInformation: record.substr(91, 20),
-          transferClassification: record.substr(111, 1),
-          identificationdisplay: record.substr(112, 1),
-          dummy: record.substr(113, 7),
-        };
-
-        // 識別表示欄にYを付与した場合はEDI情報を採用する。
-        if (obj.identificationdisplay === 'Y') {
-          obj.customerCodeOne = '';
-          obj.customerCodeTwo = '';
-        } else {
-          obj.EDIInformation = '';
-        }
-        return obj;
-      });
+      return this.items.map((record) => ({
+        dataPartition: record.substr(0, 1),
+        destinationBankNumber: record.substr(1, 4),
+        destinationBankName: record.substr(5, 15),
+        destinationBranchNumber: record.substr(20, 3),
+        destinationBranchName: record.substr(23, 15),
+        clearingHouseNumber: record.substr(38, 4),
+        depositItem: record.substr(42, 1),
+        accountNumber: record.substr(43, 7),
+        beneficiaryName: record.substr(50, 30),
+        transferAmountOfMoney: record.substr(80, 10),
+        newCode: record.substr(90, 1),
+        EDIInformation: record.substr(91, 20),
+        transferDesignationCategory: record.substr(111, 1),
+        identificationdisplay: record.substr(112, 1),
+        dummy: record.substr(113, 7),
+      }));
     },
   },
 
   data: () => ({
     headers: [
       { text: 'データ区分', value: 'dataPartition', sortable: false },
-      { text: '被仕向金融機関番号', value: 'destinationFinancialInstitutionNumber', sortable: false },
-      { text: '被仕向金融機関名', value: 'destinationFinancialInstitutionName', sortable: false },
+      { text: '被仕向銀行番号', value: 'destinationBankNumber', sortable: false },
+      { text: '被仕向銀行名', value: 'destinationBankName', sortable: false },
       { text: '被仕向支店番号', value: 'destinationBranchNumber', sortable: false },
       { text: '被仕向支店名', value: 'destinationBranchName', sortable: false },
       { text: '手形交換所番号 ', value: 'clearingHouseNumber', sortable: false },
@@ -65,10 +52,8 @@ export default {
       { text: '受取人名', value: 'beneficiaryName', sortable: false },
       { text: '振込金額', value: 'transferAmountOfMoney', sortable: false },
       { text: '新規コード', value: 'newCode', sortable: false },
-      { text: '顧客コード1', value: 'customerCodeOne', sortable: false },
-      { text: '顧客コード2', value: 'customerCodeTwo', sortable: false },
       { text: 'EDI情報', value: 'EDIInformation', sortable: false },
-      { text: '振込区分', value: 'transferClassification', sortable: false },
+      { text: '振込指定区分', value: 'transferDesignationCategory', sortable: false },
       { text: '識別表示', value: 'identificationdisplay', sortable: false },
       { text: 'ダミー', value: 'dummy', sortable: false },
     ],
