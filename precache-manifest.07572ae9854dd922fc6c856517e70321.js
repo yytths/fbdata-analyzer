66fdecde16ca47eafc77124a1a74aaf9d643e22d
yytths <yytths@users.noreply.github.com>
2020-05-22 04:03:17 +0000

    deploy: cd55161fda8627501afc9f1a721d3fffad42e88a

---
+++ precache-manifest.07572ae9854dd922fc6c856517e70321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ceea346451e7baaeda",
+    "url": "/css/chunk-vendors.d9107773.css"
+  },
+  {
+    "revision": "18193d75cf2fdcc394ea64700bb9cc44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c820a69585fdb32e63ad",
+    "url": "/js/app.bfd2da0a.js"
+  },
+  {
+    "revision": "d0ceea346451e7baaeda",
+    "url": "/js/chunk-vendors.fd8b4d93.js"
+  },
+  {
+    "revision": "e466b9ebc663043194959a0a528a9a72",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
