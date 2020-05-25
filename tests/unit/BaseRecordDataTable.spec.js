e100bf80a8285dace75cc7810e08554ea8508b76
yytths <hmdob.arch@gmail.com>
2020-05-25 15:51:58 +0900

    test: baseのテスト追加 (#12)
    * test: 1record normal test
    
    * test: vue/test-utilsのverup
    
    * test: ２レコード目もassert
    
    * test: add test no search message

--- tests/unit/BaseRecordDataTable.spec.js
+++ tests/unit/BaseRecordDataTable.spec.js
@@ -15,20 +15,97 @@ describe('BaseRecordDataTable.vue', () => {
   beforeEach(() => {
     vuetify = new Vuetify();
   });
-  it('normal test. no record message.', () => {
-    const expected = 'レコードがありません';
 
+  it('normal test. 1 records.', () => {
+    const headers = [
+      {
+        text: 'テキスト1',
+        value: 'textOne',
+      },
+      {
+        text: 'テキスト2',
+        value: 'textTwo',
+      },
+    ];
+    const items = [
+      {
+        textOne: 1,
+        textTwo: 2,
+      }
+    ];
+    const wrapper = mount(BaseRecordDataTable, {
+      localVue,
+      vuetify,
+      propsData: {
+        headers,
+        items,
+      }
+    });
+    // With jest we can create snapshot files of the HTML output
+    expect(wrapper.html()).toMatchSnapshot();
+
+    const headerElements = wrapper.findAll('.v-data-table-header > tr > th');
+    const valueElements = wrapper.findAll('.v-data-table__wrapper > table > tbody > tr > td');
+
+    expect(headerElements.at(0).find('span').text()).toBe('テキスト1');
+    expect(valueElements.at(0).text()).toBe('1');
+    expect(headerElements.at(1).find('span').text()).toBe('テキスト2');
+    expect(valueElements.at(1).text()).toBe('2');
+  });
+
+  it('normal test. no record message.', () => {    
     const wrapper = mount(BaseRecordDataTable, {
       localVue,
       vuetify,
     });
     // With jest we can create snapshot files of the HTML output
     expect(wrapper.html()).toMatchSnapshot();
-
+    
     const noDataText = wrapper.find('.v-data-table__empty-wrapper > td');
-    expect(noDataText.text()).toBe(expected);
+    const noDataMessage = 'レコードがありません';
+    expect(noDataText.text()).toBe(noDataMessage);
+  });
 
-    // TODO: WARNをなくすために、@vue/test-utils@1.0.0に更新する。
-    // https://github.com/vuejs/vue-test-utils/issues/1407
+  it('normal test. no search message.', async () => {
+    const headers = [
+      {
+        text: 'テキスト1',
+        value: 'textOne',
+      },
+      {
+        text: 'テキスト2',
+        value: 'textTwo',
+      },
+    ];
+    const items = [
+      {
+        textOne: 1,
+        textTwo: 2,
+      }
+    ];
+    const wrapper = mount(BaseRecordDataTable, {
+      localVue,
+      vuetify,
+      propsData: {
+        headers,
+        items,
+      }
+    });
+
+    // input
+    const ele = wrapper.find('#input-42');
+    ele.setValue('no-search');
+    await wrapper.vm.$nextTick();
+
+    // With jest we can create snapshot files of the HTML output
+    expect(wrapper.html()).toMatchSnapshot();
+
+    const headerElements = wrapper.findAll('.v-data-table-header > tr > th');
+    expect(headerElements.at(0).find('span').text()).toBe('テキスト1');
+    expect(headerElements.at(1).find('span').text()).toBe('テキスト2');
+
+    const noSearchMessage = 'レコードがみつかりません';
+    const noDataText = wrapper.find('.v-data-table__empty-wrapper > td');
+    expect(noDataText.text()).toBe(noSearchMessage);
   });
 });
