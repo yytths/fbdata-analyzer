9b99e034c8b50b006fc0aa11b2dff114ac2c86e4
yytths <hmdob.arch@gmail.com>
2020-05-25 17:34:36 +0900

    test: invalid input テスト

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -39,4 +39,56 @@ describe('App.vue', () => {
     expect(tabsElements.at(5).text()).toBe('不明');
   });
 
+  it('abnormal test. invalid input', async () => {
+    const wrapper = mount(App, {
+      localVue,
+      vuetify,
+    });
+
+    // ヘッダレコード
+    await _inputTextarea(wrapper, _createRandomInputTestData('1'.padEnd(9, '0')));
+    _assertInputareaErrorMessage(wrapper, 'ヘッダレコードが120文字ではありません。');
+    await _inputTextarea(wrapper, _createRandomInputTestData('1'.padEnd(11, '0')));
+    _assertInputareaErrorMessage(wrapper, 'ヘッダレコードが120文字ではありません。');
+    // データレコード
+    await _inputTextarea(wrapper, _createRandomInputTestData('2'.padEnd(9, '0')));
+    _assertInputareaErrorMessage(wrapper, 'データレコードが120文字ではありません。');
+    await _inputTextarea(wrapper, _createRandomInputTestData('2'.padEnd(11, '0')));
+    _assertInputareaErrorMessage(wrapper, 'データレコードが120文字ではありません。');
+    // トレーラレコード
+    await _inputTextarea(wrapper, _createRandomInputTestData('8'.padEnd(9, '0')));
+    _assertInputareaErrorMessage(wrapper, 'トレーラレコードが120文字ではありません。');
+    await _inputTextarea(wrapper, _createRandomInputTestData('8'.padEnd(11, '0')));
+    _assertInputareaErrorMessage(wrapper, 'トレーラレコードが120文字ではありません。');
+    // エンドレコード
+    await _inputTextarea(wrapper, _createRandomInputTestData('9'.padEnd(9, '0')));
+    _assertInputareaErrorMessage(wrapper, 'エンドレコードが120文字ではありません。');
+    await _inputTextarea(wrapper, _createRandomInputTestData('9'.padEnd(11, '0')));
+    _assertInputareaErrorMessage(wrapper, 'エンドレコードが120文字ではありません。');
+
+    // レコードタイプが未知
+    await _inputTextarea(wrapper, _createRandomInputTestData('unknown'.padEnd(10, '0')));
+    _assertInputareaErrorMessage(wrapper, '不明なデータ区分のレコードがあります。');
+  });
+
+  const _inputTextarea = async (wrapper, inputText) => {
+    // input
+    const textareaElement = wrapper.find('.v-input__slot > div > textarea');
+    textareaElement.setValue(inputText);
+    await wrapper.vm.$nextTick();
+  }
+
+  const _assertInputareaErrorMessage = (wrapper, errMessage) => {
+    const errMessageElement = wrapper.find('.v-messages__message');
+    expect(errMessageElement.exists()).toBe(true);
+    expect(errMessageElement.text()).toBe(errMessage);
+  }
+
+  const _createRandomInputTestData = (prefix = '0123456789') => {
+    return [
+      prefix,
+      [...Array(11).keys()].map(() => [...Array(10).keys()]).flat().join(''),　// 110文字の文字列
+    ].join('');
+  }
+
 });
